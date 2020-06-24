13fd4de1a976e0725d864567ffd013dd284fa84f
Vineet Joshi <vineet0713@gmail.com>
2020-06-24 11:01:11 -0700

    Display 2 columns of mat-cards using mat-grid-list.

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import {
 	MatToolbarModule,
 	MatCardModule,
+	MatGridListModule,
 } from '@angular/material';
 
 @NgModule({
 	exports: [
 		MatToolbarModule,
 		MatCardModule,
+		MatGridListModule,
 	],
 })
 export class AngularMaterialModule { }
--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -9,25 +9,31 @@ import { Item } from './../item.model';
 })
 export class ItemListComponent {
 	items = [
-		{
-			title: 'Test Artwork 1',
-			imagePath: 'https://vistapointe.net/images/artwork-2.jpg',
-		},
-		{
-			title: 'Test Artwork 2',
-			imagePath: 'https://i.etsystatic.com/18641759/r/il/d1d79e/1916230477/il_1588xN.1916230477_i5e7.jpg',
-		},
-		{
-			title: 'Test Artwork 3',
-			imagePath: 'https://cdn.hpm.io/wp-content/uploads/2019/01/18144635/2019-Marathon-Artwork-JanaviFolmsbee-AFletcher-1000x667.jpg',
-		},
-		{
-			title: 'Test Artwork 4',
-			imagePath: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/sunset-boat-landscape-artwork-painting-andres-ramos.jpg',
-		},
-		{
-			title: 'Test Artwork 5',
-			imagePath: 'https://royalthaiart.com/wp-content/uploads/2019/07/peacock-framed-art.jpg',
-		},
+		[
+			{
+				title: 'Test Artwork 1',
+				imagePath: 'https://vistapointe.net/images/artwork-2.jpg',
+			},
+			{
+				title: 'Test Artwork 2',
+				imagePath: 'https://i.etsystatic.com/18641759/r/il/d1d79e/1916230477/il_1588xN.1916230477_i5e7.jpg',
+			},
+		],
+		[
+			{
+				title: 'Test Artwork 3',
+				imagePath: 'https://cdn.hpm.io/wp-content/uploads/2019/01/18144635/2019-Marathon-Artwork-JanaviFolmsbee-AFletcher-1000x667.jpg',
+			},
+			{
+				title: 'Test Artwork 4',
+				imagePath: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/sunset-boat-landscape-artwork-painting-andres-ramos.jpg',
+			},
+		],
+		[
+			{
+				title: 'Test Artwork 5',
+				imagePath: 'https://royalthaiart.com/wp-content/uploads/2019/07/peacock-framed-art.jpg',
+			},
+		],
 	];
 }
